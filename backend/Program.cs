db3186db0585c0a0bcfe7df6e305c895d26df114
Rafi1012 <rafiel101265@gmail.com>
2026-08-08 10:20:20 -0400

    Update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -51,6 +51,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
